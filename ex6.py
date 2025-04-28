960d6467b8c328b43f70a5f128becbd94b37e17b
Schutzz <lucas.schutz@aluno.fmpsc.edu.br>
2025-04-27 23:27:54 -0300

    Update ex6.py

--- ex6.py
+++ ex6.py
@@ -1,6 +1,19 @@
-#Faça um algoritmo para ler: a descrição do produto (nome), a quantidade adquirida e o
-#preço unitário. Calcular e escrever o total (total = quantidade adquirida * preço unitário) e o
-#total a pagar (total a pagar = total - desconto), sabendo-se que:
-#- Se quantidade <= 5 o desconto será de 2%
-#- Se quantidade > 5 e quantidade <=10 o desconto será de 3%
-#- Se quantidade > 10 o desconto será de 5%
+descricao = str(input("Insira a descrição do produto: "))
+qtd_adq = float(input("Insira a quantidade adquirida: "))
+preco_unit = float(input("Insira o preço unitário: "))
+
+total = qtd_adq * preco_unit
+
+if qtd_adq <= 5:
+    desconto = 0.02 * total
+elif qtd_adq > 5 and qtd_adq <= 10:
+    desconto = 0.03 * total
+elif qtd_adq > 10:
+    desconto = 0.05 * total
+else:
+    desconto = 0 * total
+    print ("Quantidade de produtos inválida, por favor verifique se os dados estão corretos")
+
+total_a_pagar = total - desconto
+
+print (f"Descrição: {descricao} \n Quantidade adquirida: {qtd_adq} \n Preço unitário: {preco_unit} \n Total sem desconto: {total} \n Desconto: {desconto} \n Total a pagar: {total_a_pagar}")
