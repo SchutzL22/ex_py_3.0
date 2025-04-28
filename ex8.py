195205fb9d8dd422bec27c4e1cfa785d34059469
Schutzz <lucas.schutz@aluno.fmpsc.edu.br>
2025-04-27 23:47:22 -0300

    Update ex8.py

--- ex8.py
+++ ex8.py
@@ -1,2 +1,12 @@
-#Elabore um algoritmo que leia dois números inteiros e mostre o resultado da diferença do
-#maior valor pelo menor;
+print("Insira dois números inteiros diferentes:")
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+if num1 > num2:
+    resultado = num1 - num2
+elif num2 > num1:
+    resultado = num2 - num1
+else:
+    resultado = "inválido"
+
+print(f"A diferença é:{resultado}")
